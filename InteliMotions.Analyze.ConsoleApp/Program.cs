70be088fab8c8eb7524adc1ff3a637c0d51c92a5
Bruno Aguiar Daguis <bruno.daguis@multilaser.com.br>
2019-03-30 22:28:44 -0300

    Watson quase pronto

--- InteliMotions.Analyze.ConsoleApp/Program.cs
+++ InteliMotions.Analyze.ConsoleApp/Program.cs
@@ -5,6 +5,10 @@ namespace InteliMotions.Analyze.ConsoleApp
     {
         static void Main(string[] args)
         {
+
+
+
+            new InteliMotions.Analyze.Facebook.Watson.Connect().Main();
             Facebook.Connect _coonnect = new Facebook.Connect();
 
             _coonnect.GetFacebookLoginUrl();
--- InteliMotions.Analyze.Facebook.Watson/Connect.cs
+++ InteliMotions.Analyze.Facebook.Watson/Connect.cs
@@ -1,4 +1,5 @@
-﻿using IBM.WatsonDeveloperCloud.NaturalLanguageUnderstanding.v1.Model;
+﻿using IBM.WatsonDeveloperCloud.NaturalLanguageUnderstanding.v1;
+using IBM.WatsonDeveloperCloud.NaturalLanguageUnderstanding.v1.Model;
 using IBM.WatsonDeveloperCloud.Util;
 using System;
 using System.Collections.Generic;
@@ -10,18 +11,17 @@ namespace InteliMotions.Analyze.Facebook.Watson
 {
     public class Connect
     {
-        private object _naturalLanguageUnderstandingService;
 
         public void Main()
         {
 
 
             //Gerando primeiro token
-            //TokenOptions iamAssistantTokenOptions = new TokenOptions()
-            //{
-            //    IamApiKey = "<iam-apikey>",
-            //    ServiceUrl = "<service-endpoint>"
-            //};
+            TokenOptions iamAssistantTokenOptions = new TokenOptions()
+            {
+                IamApiKey = "P1CQWyM1NfM6Zy2YPqdIByyb8EgGUEhvfb05getpaNtx",
+                ServiceUrl = "https://gateway.watsonplatform.net/natural-language-understanding/api"
+            };
 
 
             //AssistantService assistantService = new AssistantService();
@@ -29,22 +29,23 @@ namespace InteliMotions.Analyze.Facebook.Watson
 
             //_assistant = new AssistantService(iamAssistantTokenOptions, "<version-date>");
 
+            NaturalLanguageUnderstandingService _naturalLanguageUnderstandingService = new NaturalLanguageUnderstandingService(iamAssistantTokenOptions, "30 DE MAR DE 2019 - 09:11:59 PM");
+            
+            Parameters parameters = new Parameters()
+            {
+                Text = "Oi eu sou um teste de programadores do brasil, isso quer dizer que vamos escutar musica e programar todo o conteudo.",
+                Features = new Features()
+                {
+                    Keywords = new KeywordsOptions()
+                    {
+                        Limit = 8,
+                        Sentiment = true,
+                        Emotion = true
+                    }
+                }
+            };
 
-            //Parameters parameters = new Parameters()
-            //{
-            //    Text = "",
-            //    Features = new Features()
-            //    {
-            //        Keywords = new KeywordsOptions()
-            //        {
-            //            Limit = 8,
-            //            Sentiment = true,
-            //            Emotion = true
-            //        }
-            //    }
-            //};
-
-            //var result = _naturalLanguageUnderstandingService.Analyze(parameters);
+            var result = _naturalLanguageUnderstandingService.Analyze(parameters);
         }
 
         
