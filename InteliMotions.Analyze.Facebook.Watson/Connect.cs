b76da325e6e307dc32884fa347056d11859009a7
Bruno Aguiar Daguis <bruno.daguis@multilaser.com.br>
2019-03-30 22:05:41 -0300

    using do watson com alguns exemplo

--- InteliMotions.Analyze.Facebook.Watson/Connect.cs
+++ InteliMotions.Analyze.Facebook.Watson/Connect.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using IBM.WatsonDeveloperCloud.NaturalLanguageUnderstanding.v1.Model;
+using IBM.WatsonDeveloperCloud.Util;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,45 @@ using System.Threading.Tasks;
 
 namespace InteliMotions.Analyze.Facebook.Watson
 {
-    class Connect
+    public class Connect
     {
+        private object _naturalLanguageUnderstandingService;
+
+        public void Main()
+        {
+
+
+            //Gerando primeiro token
+            //TokenOptions iamAssistantTokenOptions = new TokenOptions()
+            //{
+            //    IamApiKey = "<iam-apikey>",
+            //    ServiceUrl = "<service-endpoint>"
+            //};
+
+
+            //AssistantService assistantService = new AssistantService();
+            //var listWorkspacesResult = assistantService.ListWorkspaces();
+
+            //_assistant = new AssistantService(iamAssistantTokenOptions, "<version-date>");
+
+
+            //Parameters parameters = new Parameters()
+            //{
+            //    Text = "",
+            //    Features = new Features()
+            //    {
+            //        Keywords = new KeywordsOptions()
+            //        {
+            //            Limit = 8,
+            //            Sentiment = true,
+            //            Emotion = true
+            //        }
+            //    }
+            //};
+
+            //var result = _naturalLanguageUnderstandingService.Analyze(parameters);
+        }
+
+        
     }
 }
