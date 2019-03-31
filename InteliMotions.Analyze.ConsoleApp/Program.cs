1f91125cc0a183ee45f3b3ece1ba6d67178e09d0
Bruno Aguiar Daguis <bruno.daguis@gmail.com>
2019-03-30 21:07:23 -0300

    init

---
+++ InteliMotions.Analyze.ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿
+namespace InteliMotions.Analyze.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Facebook.Connect _coonnect = new Facebook.Connect();
+
+            _coonnect.GetFacebookLoginUrl();
+        }
+    }
+}
---
+++ InteliMotions.Analyze.Facebook/Connect.cs
@@ -0,0 +1,16 @@
+﻿using Facebook;
+
+namespace InteliMotions.Analyze.Facebook
+{
+    public class Connect
+    {
+        public void GetFacebookLoginUrl()
+        {
+            FacebookClient client = new FacebookClient("9bc127f0540eaf272447d465b2f3136b");
+            var result = client.Get("me/feed");
+
+
+
+        }
+    }
+}
